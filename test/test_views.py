ca4029c2e0504adefc7f292b614309c779c1403e
DanielRataj1 <DanielRataj1>
2023-04-22 23:20:47 +0200

    Korekta testów

--- test/test_views.py
+++ test/test_views.py
@@ -15,4 +15,4 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEqual(b'{ "imie":"Daniel", "mgs":"Rataj"}', rv.data)
+        self.assertEqual(b'{ "imie":"Daniel", "msg":Hello World!"}', rv.data)
